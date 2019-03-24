c422ff8db1f128f41d9278c3fd4e4befc3b8e4e9
aghilas <haddoucheaghilas@gmail.com>
2019-03-25 00:44:11 +0100

    finish todolist

---
+++ todo_list/src/Todolist.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'React'
+
+class Todolist extends Component {
+
+  render(){
+    return(
+      <h1> Ma TodoList   </h1>
+
+    )
+
+
+  }
+
+
+}
+export default Todolist;
